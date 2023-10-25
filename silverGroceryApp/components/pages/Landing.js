f1eeaf4fee65e463bb9a2e103781efe4dc6054b0
KSUProgrammer1684 <98615805+KSUProgrammer1684@users.noreply.github.com>
2023-10-25 12:51:45 -0400

    Database for android
    Setup .jason file for android database.

--- silverGroceryApp/components/pages/Landing.js
+++ silverGroceryApp/components/pages/Landing.js
@@ -15,7 +15,7 @@ function Landing() {
             {/* CREATE the image logo */}
             <Image style={styles.logoStyle} source={{ uri: "https://cdn.pixabay.com/photo/2016/03/31/23/37/bird-1297727_1280.png" }} />
             {/* TEXT element for heading */}
-            <Text style={styles.textStyle}>Silver's Grocery List</Text>
+            <Text style={styles.textStyle}>Silvers' Grocery List</Text>
             {/* CREATE a container a container for Buttons */}
             <View style={styles.buttonContainer}>
                 {/* CREATE button for Log In */}
